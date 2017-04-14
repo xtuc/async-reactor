759832ffb2193513ab411b23079f1c5576baece7
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-04-14 13:13:33 +0200

    feat: use loader in demo

--- examples/React-basic/src/App.js
+++ examples/React-basic/src/App.js
@@ -3,6 +3,17 @@ import './App.css';
 import React from 'react'
 import {asyncReactor} from 'async-reactor';
 
+function Loader() {
+
+  return (
+    <div className='container'>
+      <div className='box'>
+        <h2>Loading ...</h2>
+      </div>
+    </div>
+  );
+}
+
 async function AsyncPosts() {
   const data = await fetch('https://jsonplaceholder.typicode.com/posts');
   const posts = await data.json();
@@ -11,7 +22,7 @@ async function AsyncPosts() {
     <div className='container'>
       {posts.map((post) => (
 
-        <article key={post.id}>
+        <article key={post.id} className='box'>
           <h2>{post.title}</h2>
 
           <p>{post.body}</p>
@@ -22,4 +33,4 @@ async function AsyncPosts() {
   );
 }
 
-export default asyncReactor(AsyncPosts);
+export default asyncReactor(AsyncPosts, Loader);
